72232dc0d6685d21c83bf7cd53ac47d9ad75ae46
dims101 <dimas.stmp@gmail.com>
2025-10-24 14:33:18 +0700

    fix order seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,13 +18,13 @@ class DatabaseSeeder extends Seeder
         $this->call(PlannerGroupSeeder::class);
         $this->call(DepartmentSeeder::class);
         $this->call(AdminUserSeeder::class);
-        $this->call(EquipmentSeeder::class);
-        $this->call(FunctionalLocationSeeder::class);
-        $this->call(MatsSeeder::class);
-        $this->call(OrdersSeeder::class);
         $this->call(PlantSeeder::class);
-        $this->call(PreventiveMaintenanceSeeder::class);
         $this->call(ResourceSeeder::class);
+        $this->call(FunctionalLocationSeeder::class);
+        $this->call(EquipmentSeeder::class);
+        $this->call(OrdersSeeder::class);
+        $this->call(MatsSeeder::class);
+        $this->call(PreventiveMaintenanceSeeder::class);
         $this->call(SparepartSeeder::class);
         $this->call(UserDummySeeder::class);
 
